c4f07a5aa028ab30832130722fcc0477e0236542
suye0n1 <118587920+suye0n1@users.noreply.github.com>
2023-06-06 15:18:12 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int index = 0;
+        for(int i=1; i<=n; i++){
+            if(i%2!=0){
+                answer[index] = i;
+                index++;
+            }
+        }
+        return answer;
+    }
+}
