361ba38c6d60e08ce9e7d137827d9f19320d5deb
suye0n1 <118587920+suye0n1@users.noreply.github.com>
2023-06-24 11:33:02 +0900

    [unrated] Title: 5명씩, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(names.length+4)/5];
+        for(int i=0; i<names.length; i++){
+            if(i%5==0){
+                answer[i/5] = names[i];
+            }
+        }
+        return answer;
+    }
+}
